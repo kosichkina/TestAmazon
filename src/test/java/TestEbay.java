2a255fca9f7dd7684fd7c02b583971e10b5bf7b1
kosichkina <nataliia.poladova@gmail.com>
2022-06-19 00:11:54 +0300

    How many items for 1 Evro on Ebay

--- src/test/java/TestEbay.java
+++ src/test/java/TestEbay.java
@@ -17,15 +17,18 @@ public class TestEbay {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get(url);
+
         driver.findElement(By.cssSelector("#gh-ac.gh-tb")).sendKeys("electronica");
         driver.findElement(By.cssSelector("#gh-btn")).click();
         driver.findElement(By.xpath("//input[contains(@id, \"s0-50-12-0-1-2-6-0-6[3]-0-textrange-beginParamValue-textbox\")]")).sendKeys("1");
         driver.findElement(By.xpath("//input[contains(@id, \"s0-50-12-0-1-2-6-0-6[3]-0-textrange-endParamValue-textbox\")]")).sendKeys("1");
         driver.findElement(By.cssSelector(".x-textrange__button")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
         WebElement amount1evro = driver.findElement(By.cssSelector("div h1.srp-controls__count-heading span.BOLD:first-child"));
         Assert.assertTrue(amount1evro.isDisplayed());
-        String par = amount1evro.getText();
-        System.out.println(par);
+
+        String quantity = amount1evro.getText();
+        System.out.println(quantity);
     }
 }
