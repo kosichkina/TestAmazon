a7cd3d58701ee2560d0a24462702d3707165537b
kosichkina <nataliia.poladova@gmail.com>
2022-06-18 20:28:59 +0300

    Used Function

--- src/test/java/TestAmazon.java
+++ src/test/java/TestAmazon.java
@@ -8,13 +8,13 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class TestAmazon {
+    WebDriver driver;
+    String url = "https://www.amazon.es/";
 
-    @Test
-
-    public void AmazonCheckTest(){
+    public void checkInStock(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.es/");
+        driver = new ChromeDriver();
+        driver.get(url);
         WebElement acceptButton = driver.findElement(By.xpath("//input[contains(@id, \"sp-cc-accept\")]"));
         if(acceptButton.isDisplayed()){
             acceptButton.click();
@@ -25,10 +25,29 @@ public class TestAmazon {
         driver.findElement(By.xpath("//a[contains(@aria-label, \"Abrir menú\")]")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//a[contains(@href, \"nav_em_cs_newreleases_0_1_1_3\")][contains(@class, \"hmenu-item\")]")).click();
+    }
+
+
+    @Test
+
+    public void AmazonCheckTest(){
+        checkInStock();
         driver.findElement(By.xpath("//a[contains(@href, \"/gp/new-releases/kitchen\")][contains(@class, \"a-link-normal\")]")).click();
         driver.findElement(By.xpath("//div[contains(@id, \"B0B28G8XCN\")]")).click();
-        WebElement enStock = driver.findElement(By.xpath("//span[contains(@class, \"a-size-medium a-color-success\")]"));
-        Assert.assertTrue(enStock.isDisplayed());
+        WebElement addToCart = driver.findElement(By.xpath("//input[contains(@id, \"add-to-cart-button\")]"));
+        Assert.assertTrue(addToCart.isDisplayed());
 
     }
+
+    @Test
+    public void AmazonCheck2(){
+        checkInStock();
+        driver.findElement(By.xpath("//a[contains(@href, \"/gp/new-releases/shoes\")][contains(@class, \"a-link-normal\")]")).click();
+        driver.findElement(By.xpath("//div[contains(@id, \"B0B21W9XYT\")]")).click();
+        WebElement buyNow = driver.findElement(By.xpath("//input[contains(@id, \"buy-now-button\")]"));
+        Assert.assertTrue(buyNow.isDisplayed());
+
+    }
+
+
 }
